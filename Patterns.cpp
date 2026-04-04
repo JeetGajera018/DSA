1e1d4285e5e7b8a973b922aa49f0092e3baaf142
JeetGajera <jeetgajera018@gmail.com>
2026-04-04 14:36:51 +0530

    Add files via upload

--- Patterns.cpp
+++ Patterns.cpp
@@ -12,7 +12,6 @@ void print1(int n)
         cout << endl;
     }
 }
-
 void print2(int n)
 {
     for (int i = 0; i < n; i++)
@@ -24,7 +23,6 @@ void print2(int n)
         cout << endl;
     }
 }
-
 void print3(int n)
 {
     for (int i = 1; i <= n; i++)
@@ -36,7 +34,6 @@ void print3(int n)
         cout << endl;
     }
 }
-
 void print4(int n)
 {
     for (int i = 1; i <= n; i++)
@@ -48,7 +45,6 @@ void print4(int n)
         cout << endl;
     }
 }
-
 void print5(int n)
 {
     for (int i = 1; i <= n; i++)
@@ -60,7 +56,6 @@ void print5(int n)
         cout << endl;
     }
 }
-
 void print6(int n)
 {
     for (int i = 1; i <= n; i++)
@@ -72,7 +67,6 @@ void print6(int n)
         cout << endl;
     }
 }
-
 void print7(int n)
 {
     for (int i = 0; i < n; i++)
@@ -94,7 +88,6 @@ void print7(int n)
         cout << endl;
     }
 }
-
 void print8(int n)
 {
     for (int i = 0; i < n; i++)
@@ -116,7 +109,6 @@ void print8(int n)
         cout << endl;
     }
 }
-
 void print10(int n)
 {
     for (int i = 1; i <= 2 * n - 1; i++)
@@ -131,11 +123,266 @@ void print10(int n)
         cout << endl;
     }
 }
+void print11(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int x = 1;
+        if (i % 2 == 0)
+        {
+            x = 0;
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            cout << x << "";
+            x = 1 - x;
+        }
+        cout << endl;
+    }
+}
+void print12(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        int space = 2 * (n - i);
 
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+
+        for (int j = 1; j <= space; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j;
+        }
+        cout << endl;
+        space -= 2;
+    }
+}
+void print13(int n)
+{
+    int num = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << num << " ";
+            num += 1;
+        }
+        cout << endl;
+    }
+}
+void print14(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'A'; ch <= 'A' + i; ch++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+void print15(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'A'; ch <= 'A' + (n - i - 1); ch++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+void print16(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        char ch = 'A' + i;
+        for (int j = 0; j <= i; j++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+void print17(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+
+        char ch = 'A';
+        int breakpoint = (2 * i + 1) / 2;
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << ch << "";
+            if (j < breakpoint)
+            {
+                ch++;
+            }
+            else
+            {
+                ch--;
+            }
+        }
+
+        for (int j = 0; j < n - i + 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+void print18(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'E' - i; ch <= 'E'; ch++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+void print19(int n)
+{
+    int iniS = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "*";
+        }
+
+        for (int j = 0; j < iniS; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "*";
+        }
+        iniS += 2;
+        cout << endl;
+    }
+
+    iniS = 2 * (n - 1);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+
+        for (int j = 0; j < iniS; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        iniS -= 2;
+        cout << endl;
+    }
+}
+void print20(int n)
+{
+    int space = 2 * n - 2;
+    for (int i = 1; i <= 2 * n - 1; i++)
+    {
+        int stars = i;
+        if (i > n)
+            stars = 2 * n - i;
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << "*";
+        }
+
+        for (int j = 1; j <= space; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= stars; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+        if (i < n)
+            space -= 2;
+        else
+            space += 2;
+    }
+}
+void print21(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == 0 || j == 0 || i == n - 1 || j == n - 1)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+void print22(int n)
+{
+    for (int i = 0; i < 2 * n - 1; i++)
+    {
+        for (int j = 0; j < 2 * n - 1; j++)
+        {
+            int top = i;
+            int left = j;
+            int bottom = (2 * n - 2) - i;
+            int right = (2 * n - 2) - j;
+            cout << (n - min(min(top, bottom), min(left, right)));
+        }
+        cout << endl;
+    }
+}
 int main()
 {
 
     int n;
     cin >> n;
+    print1(n);
+    print2(n);
+    print3(n);
+    print4(n);
+    print5(n);
+    print6(n);
+    print7(n);
+    print8(n);
     print10(n);
+    print11(n);
+    print12(n);
+    print13(n);
+    print14(n);
+    print15(n);
+    print16(n);
+    print17(n);
+    print18(n);
+    print19(n);
+    print20(n);
+    print21(n);
+    print22(n);
 }
