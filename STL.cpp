b60050ea2c0c76050c7ee712c9b21e876ea53346
JeetGajera <jeetgajera018@gmail.com>
2026-04-04 21:55:58 +0530

    Add files via upload

--- STL.cpp
+++ STL.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// CONTAINERS
+
 // pairs
 void pairs()
 {
@@ -17,7 +19,6 @@ void pairs()
     pair<int, int> arr[] = {{1, 2}, {3, 4}, {5, 6}};
     cout << arr[1].second;
 }
-
 // vectors
 void vectors()
 {
@@ -115,7 +116,6 @@ void vectors()
     // to check the vector is empty or not
     cout << v3.empty();
 }
-
 // list
 void lists()
 {
@@ -131,13 +131,11 @@ void lists()
 
     // all the fuctions are same as the vector
 }
-
 // deque
 void deques()
 {
     // all the fuctions are similar thi list and vector
 }
-
 // stack = LIFO (last in first out)
 void stacks()
 {
@@ -158,7 +156,6 @@ void stacks()
     stack<int> st1, st2;
     st1.swap(st2);
 }
-
 // queue =FIFO(first in first out)
 void queues()
 {
@@ -176,7 +173,6 @@ void queues()
     cout << qu.front() << endl;
     // size,empty,swap same as stack
 }
-
 // priority_queue = the guy has the highest value stays at the top
 void priority_queues()
 {
@@ -205,8 +201,157 @@ void priority_queues()
     pq2.pop();
     cout << pq2.top() << endl;
 }
+// set = stores everything in sorted order and stores unique
+void sets()
+{
+    set<int> s;
+    s.insert(1);
+    s.emplace(3);
+    s.insert(3);
+    s.insert(4);
+    s.emplace(2);
+
+    // iterator in set
+    auto it = s.find(1); // to find the  memory address of 1
+    auto it2 = s.find(3);
+    // to erase
+    s.erase(4);
+    s.erase(it, it2);
+
+    // to check the element is in the set or not
+    int cnt2 = s.count(2);
+
+    // lower bound andupper bound
+    auto it3 = s.lower_bound(2);
+    auto it4 = s.upper_bound(2);
+}
+// multiset = everything is same as set but doesn't stores the unique values
+void multisets()
+{
+    multiset<int> ms;
+    ms.insert(1);
+    ms.insert(1);
+    ms.insert(1);
+    ms.insert(1);
+    ms.emplace(1);
+    ms.emplace(1);
+    ms.emplace(1);
+
+    // onl;y single one is erase
+    ms.erase(ms.find(1));
+
+    // all 1 is erase at a time
+    ms.erase(1);
+
+    int cnt1 = ms.count(1);
+}
+// unorderedset = everything is unique but doesn't stores in the sorted order
+void unorderedsets()
+{
+    unordered_set<int> us;
+    // lower and upper bound function doesn't works , rest all functions works the same
+    // it has the better complexity then the set in most cases, except some when collision happens
+}
+// maps = maps is the container which stores everything in respect of keys(unique) and values. {key,value}.
+// maps stores unique keys in sorted order
+void maps()
+{
+    map<int, int> mp;
+    mp[1] = 2;
+    mp.insert({3, 4});
+    mp.emplace(5, 6);
+
+    map<pair<int, int>, int> mp2;
+    mp2[{1, 2}] = 3;
+
+    for (auto m : mp)
+    {
+        cout << m.first << " " << m.second << endl;
+    }
+
+    for (auto n : mp2)
+    {
+        cout << n.first.first << " " << n.first.second << " " << n.second << endl;
+    }
+
+    cout << mp[1] << endl;
+
+    auto it = mp.find(5);
+    cout << (*it).second;
+}
+// multimaps= similar to maps but we can store the similare keys in the sorted order
+// unrederedmaps= similar to maps but not in  the sorted order
+
+// Algorithm
+
+bool comp(pair<int, int> p1, pair<int, int> p2)
+{
+    if (p1.second < p2.second)
+        return true;
+    if (p1.second > p2.second)
+        return false;
+    // if they are same
+    if (p1.first > p2.first)
+        return true;
+    else
+        return false;
+}
+
+void sorting()
+{
+    int a[] = {3, 2, 1, 5, 4, 6};
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(5);
+    v.push_back(3);
+    v.push_back(6);
+    v.push_back(4);
+    v.emplace_back(2);
+    pair<int, int> arr[] = {{1, 2}, {3, 4}, {5, 6}};
+
+    sort(a, a + 6);
+    for (int i = 0; i < 6; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    sort(v.begin(), v.end());
+    for (auto i : v)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    // sort it according to the second element
+    // if the second element is same then
+    // sor it according to the first element but in decending order
+    sort(arr, arr + 3, comp);
+    for (auto j : arr)
+    {
+        cout << j.first << " " << j.second << endl;
+    }
+
+    // to find the number of 1 in the binary form of any number or value
+    int num = 7;
+    int cnt = __builtin_popcount(num);
+    cout << cnt << endl;
+    long long num2 = 274937932;
+    int cnt2 = __builtin_popcountll(num2);
+    cout << cnt2 << endl;
+
+    // permutation
+    string s = "345";
+    do
+    {
+        cout << s << endl;
+    } while (next_permutation(s.begin(), s.end()));
+
+    // to give the max element from the array
+    int max = *max_element(a, a + 6);
+    cout << max << endl;
+}
 
 int main()
 {
-    priority_queues();
+    sorting();
 }
